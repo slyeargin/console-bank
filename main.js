45961258e2188bbef8b9d6d18f4ca7520f9ff97b
Samantha Yeargin <slyeargin@gmail.com>
2014-04-09 21:32:35 -0500

    Forced quit when the account balance triggered closing

--- main.js
+++ main.js
@@ -7,17 +7,17 @@ while(!isQuit(response)){
   switch(response){
   case 'd':
     balance = add(x,balance);
-    operation = "added";
+    operation = "deposited into";
     break;
   case 'w':
     balance = sub(x,balance);
-    operation = "removed";
+    operation = "withdrawn from";
     break;
   default:
     console.log('That was not a valid option. Please try again.');
   }
 
-  var string = getString(x,balance,out,operation);
+  var string = getString(x,balance,operation);
   console.log(string);
   response = query();
 }
@@ -27,7 +27,8 @@ console.log("Thanks for banking with us!")
 function query() {
   console.log("Your balance is $" + balance + ".");
   if (balance < -750) {
-    console.log('Your account has been closed.')
+    console.log('Your account has been closed.');
+    return response = 'q';
   } else {
   var response = prompt("Would you like to make a (d)eposit, a (w)ithdrawal, or (q)uit?");
   return response.toLowerCase();
@@ -46,7 +47,7 @@ function isQuit(letter) {
 function add(x, balance) {
   if (x > 500) {
     x += 25;
-    console.log('As a bonus for being a loyal customer, we\'ve added $25 to your account.')
+    console.log('As a bonus for being a loyal customer, we\'ve added $25 to your account.');
   }
   return balance += x;
 }
@@ -55,11 +56,11 @@ function sub(x, balance) {
   balance -= x;
   if (balance < 0) {
     x -= 50;
-    console.log('Your account has been assessed a $50 overdraft fee.')
+    console.log('Your account has been assessed a $50 overdraft fee.');
   }
   return balance;
 }
 
 function getString(x,balance,operation) {
-  return "$" + x + " has been " + operation + " to your account.  Your balance is $" + balance + ".";
+  return "$" + x + " has been " + operation + " your account.  Your balance is $" + balance + ".";
 }
